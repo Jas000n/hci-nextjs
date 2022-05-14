b24f76cf14cc5fdc7340192984c185e548ce960b
mirrorgo <cloudmirrorgo@163.com>
2022-05-14 17:56:22 +0800

    login completed

---
+++ initial/initialUser.js
@@ -0,0 +1,5 @@
+const initialUsers = [
+  { userName: "test", password: "test", type: "test" },
+  { userName: "factory manager", password: "go", type: "factory manager" },
+];
+export { initialUsers };

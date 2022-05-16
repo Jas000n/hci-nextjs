8f73707a23c2e78a18da4cadb7e018926cab6e30
Jas000n <77543568+Jas000n@users.noreply.github.com>
2022-05-16 22:04:43 +0800

    order management for business man is finished!

--- pages/api/users.js
+++ pages/api/users.js
@@ -15,5 +15,9 @@ export default function handler(req, res) {
 
 const users = [
   { userName: "test", password: "test", type: "test" },
-  { userName: "factory manager", password: "go", type: "factory manager" },
+  { userName: "jxslb", password: "123456", type: "agency_boss" },
+  { userName: "jxsyg", password: "123456", type: "agency_employee" },
+  { userName: "gtsh", password: "123456", type: "businessman" },
+  { userName: "sccs", password: "123456", type: "manufacturer" },
+  { userName: "factory manager", password: "go", type: "factory_manager" },
 ];
--- pages/workbench/order/management/index.jsx
+++ pages/workbench/order/management/index.jsx
@@ -1,13 +1,25 @@
-import { Button } from "antd";
-import { Table } from "antd";
+import { Tabs, Table, Tag, Space, Progress, Divider, Input } from "antd";
+import { useContext } from "react";
+import { CurrentUserContext } from "../../../../context/CurrentUserContext";
 import WorkbenchLayout from "../../../../layout/WorkbenchLayout";
-
+const { Search } = Input;
 export default function Order() {
+  const [user, setUser] = useContext(CurrentUserContext);
+  console.log('user',user)
+  if(user.type==="businessman"){
+    console.log("hello businessman!")
+  }
   return (
     <div>
+      <div>
+        <Search placeholder="搜索一切" style={{ width: 200 }} />
+      </div>
+      <Divider>工作台</Divider>
       <Table dataSource={initialOrders} columns={columns} />
-      {/* <Button>+++</Button> */}
+      <Divider style={{ marginBottom: "40px" }}>数据中心</Divider>
+
     </div>
+    
   );
 }
 
@@ -18,39 +30,78 @@ Order.getLayout = function getLayout(page) {
 const initialOrders = [
   {
     key: "1",
-    name: "胡彦斌",
-    age: 32,
-    address: "西湖区湖底公园1号",
-    op: <Button>+</Button>,
+    order_id: "1",
+    order_state: "等待投标",
+    order_progress: "50",
+    order_detail:"查看投标",
+    order_content:"电脑",
   },
   {
     key: "2",
-    name: "胡彦祖",
-    age: 42,
-    address: "西湖区湖底公园1号",
-    op: <Button>+</Button>,
+    order_id: "1",
+    order_state: "等待投标",
+    order_progress: "20",
+    order_detail:"查看投标",
+    order_content:"手机",
+  },
+  {
+    key: "3",
+    order_id: "1",
+    order_state: "等待投标",
+    order_progress: "30",
+    order_detail:"查看投标",
+    order_content:"平板",
+  },
+  {
+    key: "4",
+    order_id: "1",
+    order_state: "等待投标",
+    order_progress: "80",
+    order_detail:"查看投标",
+    order_content:"鼠标",
   },
 ];
 
 const columns = [
   {
-    title: "姓名",
-    dataIndex: "name",
-    key: "name",
+    title: "订单ID",
+    dataIndex: "order_id",
+    key: "order_id",
   },
   {
-    title: "年龄",
-    dataIndex: "age",
-    key: "age",
+    title: "订单状态",
+    dataIndex: "order_state",
+    key: "order_state",
   },
   {
-    title: "住址",
-    dataIndex: "address",
-    key: "address",
+    title: "订单内容",
+    dataIndex: "order_content",
+    key: "order_content",
   },
   {
-    title: "op",
-    dataIndex: "op",
-    key: "op",
+    title: "订单进度",
+    dataIndex: "order_progress",
+    key: "order_progress",
+    render: (text, record) => {
+      return<>
+      <Progress percent={text} size="small" status="active" />
+      </>
+      
+    },
+    
+  },
+  {
+    title: "详情",
+    dataIndex: "order_detail",
+    key: "order_detail",
+    render: (text, record) => {
+      return<>
+      <Space size="middle">
+        <a>{text}</a>
+
+      </Space>
+      </>
+      
+    },
   },
 ];

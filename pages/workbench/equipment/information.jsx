3a8f56e5de978ef79b06693e0957174e1771bd94
Jas000n <77543568+Jas000n@users.noreply.github.com>
2022-05-19 11:02:11 +0800

    缺失的设备信息页面完成

--- pages/workbench/equipment/information.jsx
+++ pages/workbench/equipment/information.jsx
@@ -2,39 +2,68 @@ import { Button } from "antd";
 import { createContext } from "react";
 import { useContext } from "react";
 import WorkbenchLayout from "../../../layout/WorkbenchLayout";
-
+import { Table, Tag, Space } from 'antd';
 const ContextDemo = createContext("default");
 
 export default function Information() {
+  const columns = [
+    {
+      title: '设备名称',
+      dataIndex: 'name',
+      key: 'name',
+      render: text => <a>{text}</a>,
+    },
+    
+    {
+      title: '设备类型',
+      dataIndex: 'type',
+      key: 'type',
+    },
+    {
+      title: '设备产能',
+      key: 'productivity',
+      dataIndex: 'productivity',
+
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text, record) => (
+        <Space size="middle">
+          <a>管理{record.name}</a>
+          <a>排产</a>
+        </Space>
+      ),
+    },
+  ];
+  
+  const data = [
+    {
+      key: '1',
+      name: "西门子",
+      productivity:100,
+      type:"电冰箱",
+    },
+    {
+      key: '2',
+      name: "松下",
+      productivity:50,
+      type:"电冰箱",
+    },
+    {
+      key: '3',
+      name: "华为",
+      productivity:300,
+      type:"手机",
+    },
+  ];
+  
   return (
-    <div>
-      <ContextDemo.Provider value="wow">
-        <Demo1 />
-      </ContextDemo.Provider>
-    </div>
+    <Table columns={columns} dataSource={data} />
   );
 }
 
-function Demo1() {
-  return <Demo2 />;
-}
-function Demo2() {
-  const contextDemo = useContext(ContextDemo);
-  return (
-    <div>
-      information
-      {contextDemo}
-      <Button
-        onClick={() => {
-          // console.log(user);
-          console.log(contextDemo);
-        }}
-      >
-        user
-      </Button>
-    </div>
-  );
-}
+
 
 Information.getLayout = function getLayout(page) {
   return <WorkbenchLayout>{page}</WorkbenchLayout>;
